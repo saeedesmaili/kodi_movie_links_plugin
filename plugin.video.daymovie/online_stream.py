6389cd7d51dedf0746cd34e33a2e31b86e82a06a
Saeed <saaeed.es20@gmail.com>
2020-04-17 16:02:41 +0430

    separate online_stream functions

--- plugin.video.daymovie/online_stream.py
+++ plugin.video.daymovie/online_stream.py
@@ -320,5 +320,244 @@ def search_results(keyword, s):
         elif "movie" in href:
             items_dict["Movies"].append(this_item)
 
+    return items_dict
+
+
+def list_categories(s):
+    """
+    Create the list of video categories in the Kodi interface.
+    """
+    xbmc.log("--list_categories",level=xbmc.LOGNOTICE)
+    keyword = user_input()
+    items_dict = search_results(keyword, s)
+    # Set plugin category. It is displayed in some skins as the name
+    # of the current section.
+    xbmcplugin.setPluginCategory(_handle, 'My Video Collection')
+    # Set plugin content. It allows Kodi to select appropriate views
+    # for this type of content.
+    xbmcplugin.setContent(_handle, 'videos')
+    # Get video categories
+    categories = items_dict.iterkeys()
+    # Iterate through categories
+    for category in categories:
+        xbmc.log(category,level=xbmc.LOGNOTICE)
+        if len(items_dict[category]) == 0:
+            xbmc.log("There is no results here.",level=xbmc.LOGNOTICE)
+            continue
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=category)
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use the same image for all items for simplicity's sake.
+        # In a real-life plugin you need to set each image accordingly.
+        list_item.setArt({'thumb': items_dict[category][0]['img'],
+                          'icon': items_dict[category][0]['img'],
+                          'fanart': items_dict[category][0]['img']})
+        # Set additional info for the list item.
+        # Here we use a category name for both properties for for simplicity's sake.
+        # setInfo allows to set various information for an item.
+        # For available properties see the following link:
+        # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
+        # 'mediatype' is needed for a skin to display info for this ListItem correctly.
+        list_item.setInfo('video', {'title': category,
+                                    #'genre': category,
+                                    'mediatype': 'video'})
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=item_listing&category=Animals
+        # url = get_url(action='item_listing', category=category)
+        # is_folder = True means that this item opens a sub-list of lower level items.
+        is_folder = False
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_handle, "", list_item, is_folder)
+
+        for video in items_dict[category]:
+            xbmc.log(str(video),level=xbmc.LOGNOTICE)
+            list_item = xbmcgui.ListItem(label=video['title'])
+            list_item.setInfo('video', {'title': video['title'],
+                                        'rating': video['rating'],
+                                        #'genre': video['href'],
+                                        'mediatype': 'video'})
+            list_item.setArt({'thumb': video['img'], 'icon': video['img'], 'fanart': video['img']})
+            url = get_url(action='file_listing', url=video['href'], category=category)
+            is_folder = True
+            # Add our item to the Kodi virtual folder listing.
+            xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
 
     return items_dict
+
+
+def list_files(url, category, s):
+    xbmc.log("--list_files",level=xbmc.LOGNOTICE)
+    result_dict = dict()
+    response = s.get(url)
+
+    if category == "Movies":
+        soup_movie = BeautifulSoup(response.text, 'html.parser')
+
+        movie_download_dict = {"Download Links": []}
+
+        items = soup_movie.find_all(class_="dlbox")
+        for item in items:
+            if "دوبله" in str(item):
+                continue
+            dldetails = str(item.find(class_="dldetails"))
+            quality = re.search("کیفیت: <span>(.+?)</span>", dldetails).group(1)
+            size = re.search("حجم : <span>(.+?)</span>", dldetails).group(1).replace("گیگابایت", "GB").replace("مگابایت", "MB")
+            resolution = re.search("رزولوشن:  <span>(.+?)</span>", dldetails).group(1)
+            encoder = re.search("انکودر:  <span>(.+?)</span>", dldetails).group(1)
+            download_url = item.find(class_="dbtn")["href"]
+            screenshot_url = item.find(class_="pbtn")["href"]
+            this_item = {
+                "quality": quality,
+                "size": size,
+                "resolution": resolution,
+                "encoder": encoder,
+                "download_url": download_url,
+                "screenshot_url": screenshot_url
+            }
+            movie_download_dict["Download Links"].append(this_item)
+
+        xbmcplugin.setPluginCategory(_handle, 'My Video Collection')
+        xbmcplugin.setContent(_handle, 'videos')
+
+        xbmc.log(str(movie_download_dict),level=xbmc.LOGNOTICE)
+
+        for video in movie_download_dict["Download Links"]:
+            xbmc.log(str(video),level=xbmc.LOGNOTICE)
+            list_item = xbmcgui.ListItem(label=video['quality'])
+            list_item.setInfo('video', {'title': video['quality'],
+                                        'size': video['size'],
+                                        'mediatype': 'video'})
+            list_item.setArt({'thumb': video['screenshot_url'], 'icon': video['screenshot_url'], 'fanart': video['screenshot_url']})
+            list_item.setProperty('IsPlayable', 'true')
+            url = get_url(action='play', url=video['download_url'])
+            is_folder = False
+            # Add our item to the Kodi virtual folder listing.
+            xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+
+        result_dict = movie_download_dict
+
+    elif category == "TV Shows":
+        soup_tv = BeautifulSoup(response.text, 'html.parser')
+        tv_download_page_dict = dict()
+
+        items = soup_tv.find_all(class_="dlbox")
+        for item in items:
+            if "دوبله" in str(item):
+                continue
+            season = "Season " + re.search("فصل: <span>(.+?)</span>", str(item.find(class_="dldetails"))).group(1)
+            tv_download_page_dict[season] = []
+            content_items = item.find(class_="tvserieslinks").find_all("li", attrs={"style": "position: relative"})
+            for content_item in content_items:
+                quality = re.search("کیفیت: (.+?) </div>", str(content_item.find(class_="qlty"))).group(1)
+                href = "http://1daymovie.org" + content_item.find(class_="dbtn")["href"]
+                size = content_item.find(class_="dbtn").find("i").text.replace("M", " MB")
+                this_content_item = {
+                    "quality": quality,
+                    "size": size,
+                    "href": href,
+                }
+                tv_download_page_dict[season].append(this_content_item)
+
+        seasons = sorted(tv_download_page_dict.iterkeys())
+        
+        xbmcplugin.setPluginCategory(_handle, 'My Video Collection')
+        xbmcplugin.setContent(_handle, 'videos')
+
+        xbmc.log(str(tv_download_page_dict),level=xbmc.LOGNOTICE)
+
+        for season in seasons:
+            season_number = re.search("(\d{2})", season).group(1)
+            xbmc.log(season,level=xbmc.LOGNOTICE)
+            # Create a list item with a text label and a thumbnail image.
+            list_item = xbmcgui.ListItem(label=season)
+            # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+            # Here we use the same image for all items for simplicity's sake.
+            # In a real-life plugin you need to set each image accordingly.
+
+            # TODO: get the tv show poster from parent
+            # list_item.setArt({'thumb': items_dict[category][0]['img'],
+            #                 'icon': items_dict[category][0]['img'],
+            #                 'fanart': items_dict[category][0]['img']})
+            # Set additional info for the list item.
+            # Here we use a category name for both properties for for simplicity's sake.
+            # setInfo allows to set various information for an item.
+            # For available properties see the following link:
+            # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
+            # 'mediatype' is needed for a skin to display info for this ListItem correctly.
+            list_item.setInfo('video', {'title': season,
+                                        'genre': season,
+                                        'mediatype': 'video'})
+            # Create a URL for a plugin recursive call.
+            # Example: plugin://plugin.video.example/?action=item_listing&category=Animals
+            # url = get_url(action='item_listing', category=category)
+            # is_folder = True means that this item opens a sub-list of lower level items.
+            is_folder = False
+            # Add our item to the Kodi virtual folder listing.
+            xbmcplugin.addDirectoryItem(_handle, "", list_item, is_folder)
+
+            for video in tv_download_page_dict[season]:
+                xbmc.log(str(video),level=xbmc.LOGNOTICE)
+                list_item = xbmcgui.ListItem(label=video['quality'])
+                list_item.setInfo('video', {'title': video['quality'],
+                                            'size': video['size'],
+                                            'mediatype': 'video'})
+                # TODO: get the tv show poster from parent
+                # list_item.setArt({'thumb': video['img'], 'icon': video['img'], 'fanart': video['img']})
+
+                # ------
+                url = get_url(action='episode_listing', url=video['href'], quality=video['quality'], season_number=season_number)
+                is_folder = True
+                # Add our item to the Kodi virtual folder listing.
+                xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+
+        result_dict = tv_download_page_dict
+        
+
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+        
+    return result_dict
+
+
+def list_episodes(url, quality, season_number, s):
+    xbmc.log("--list_episodes",level=xbmc.LOGNOTICE)
+    response = s.get(url)
+    soup_tv_episodes = BeautifulSoup(response.text, 'html.parser')
+
+    items = soup_tv_episodes.find(class_="searchresults").find_all("li", attrs={"style": "direction: ltr"})
+    tv_episodes_list = []
+    for item in items:
+        tv_episodes_list.append(item.find("a")["href"])
+
+    xbmcplugin.setPluginCategory(_handle, 'My Video Collection')
+    xbmcplugin.setContent(_handle, 'videos')
+
+    xbmc.log(str(tv_episodes_list),level=xbmc.LOGNOTICE)
+
+    for video_url in tv_episodes_list:
+        try:
+            episode = "Episode: " + re.search("S" + season_number + "E(\d{2})", video_url).group(1)
+        except:
+            episode = video_url
+        xbmc.log(str(episode),level=xbmc.LOGNOTICE)
+        list_item = xbmcgui.ListItem(label=episode)
+        list_item.setInfo('video', {'title': episode,
+                                    'genre': quality,
+                                    'mediatype': 'video'})
+        #list_item.setArt({'thumb': video['screenshot_url'], 'icon': video['screenshot_url'], 'fanart': video['screenshot_url']})
+        list_item.setProperty('IsPlayable', 'true')
+        url = get_url(action='play', url=video_url)
+        is_folder = False
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+
+    xbmcplugin.endOfDirectory(_handle)
+
+    return tv_episodes_list
